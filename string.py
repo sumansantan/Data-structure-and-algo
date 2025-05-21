2861221e6464da2540710c54480c67c21b00e7bf
Santan Suman <santansuman@Santans-MacBook-Air.local>
2025-05-21 23:48:38 +0530

    added new files

---
+++ string.py
@@ -0,0 +1,14 @@
+a = "santan"
+print(len(a))
+print(a.upper())
+print(a.lower())
+b = "santan!!!!"
+print(b.rstrip("!"))
+
+aa = "santan suman"
+print(aa.split(" ")[0])
+print(aa.split(" ")[1])
+print(f" my name is : {aa}")
+print("my name is : {}".format(a.upper()))
+
+bb = "santan"
